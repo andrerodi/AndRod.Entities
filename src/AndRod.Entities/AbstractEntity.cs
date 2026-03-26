844d928e4550e154497345b59cc5a57ce10d5b90
Andre Rodi <andre.rodi@gmail.com>
2026-03-26 22:51:12 +0100

    Add .gitignore and AndRod.Entities project

---
+++ src/AndRod.Entities/AbstractEntity.cs
@@ -0,0 +1,73 @@
+﻿using AndRod.Core;
+using AndRod.StronglyTypedIds;
+
+namespace AndRod.Entities;
+
+/// <summary>
+/// Represents an abstract entity with a strongly-typed ID.
+/// </summary>
+/// <typeparam name="TSelf">The type of the entity.</typeparam>
+/// <typeparam name="TId">The type of the entity's ID.</typeparam>
+public abstract class AbstractEntity<TSelf, TId> :
+    IAbstractEntity<TSelf, TId>,
+    ITypedCloneable<TSelf>,
+    IEquatable<TSelf>,
+    IComparable<TSelf>
+    where TSelf : AbstractEntity<TSelf, TId>
+    where TId : IStronglyTypedId, IComparable<TId>
+{
+    protected AbstractEntity() { }
+
+    protected AbstractEntity(TId id)
+    {
+        Id = id;
+    }
+
+    /// <summary>
+    /// Gets the entity's ID.
+    /// </summary>
+    public TId Id { get; private set; } = StronglyTypedIdFactory.Empty<TId>();
+
+    /// <summary>
+    /// Gets the date and time the entity was created.
+    /// </summary>
+    public DateTimeOffset CreatedAt { get; private set; } = DateTimeOffset.UtcNow;
+
+    /// <summary>
+    /// Gets the date and time the entity was last updated.
+    /// </summary>
+    public DateTimeOffset? UpdatedAt { get; private set; }
+
+    /// <inheritdoc />
+    public void Update() => UpdatedAt = DateTimeOffset.UtcNow;
+
+    /// <summary>
+    /// Compares the entity's ID to another entity's ID.
+    /// </summary>
+    /// <inheritdoc />
+    public virtual int CompareTo(TSelf? other)
+    {
+        if (other is null) return 1;
+        return Id.CompareTo(other.Id);
+    }
+
+    /// <inheritdoc />
+    public abstract TSelf Clone();
+
+    /// <summary>
+    /// Compares the entity's ID to another entity's ID for equality.
+    /// </summary>
+    /// <inheritdoc />
+    public bool Equals(TSelf? other) => other is not null && Id.Equals(other.Id);
+
+    /// <summary>
+    /// Uses <see cref="TSelf.Equals(TSelf?)"/> to compare the entity's ID to another entity's ID for equality.
+    /// </summary>
+    /// <inheritdoc />
+    public override bool Equals(object? obj) => obj is TSelf other && Equals(other);
+
+    /// <summary>
+    /// Returns the hash code of the entity's ID.
+    /// </summary>
+    public override int GetHashCode() => Id.GetHashCode();
+}
---
+++ src/AndRod.Entities/IAbstractEntity.cs
@@ -0,0 +1,18 @@
+﻿using AndRod.StronglyTypedIds;
+
+namespace AndRod.Entities;
+
+public interface IAbstractEntity<TSelf, TKey>
+    where TSelf : IAbstractEntity<TSelf, TKey>
+    where TKey : IStronglyTypedId
+{
+    /// <summary>
+    /// Gets the unique identifier of the entity.
+    /// </summary>
+    TKey Id { get; }
+
+    /// <summary>
+    /// Updates the entity's <see cref="UpdatedAt"/> timestamp.
+    /// </summary>
+    public void Update();
+}
